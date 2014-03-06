6ec6ed0e46024059480fb03e3f8a685daafd1993
elizabeth ocallaghan <eocallaghan@eocallaghan-Latitude-E6430.(none)>
2014-03-06 16:59:34 -0500

    few changes from last commit just updated and added to a few functions

--- galga.py
+++ galga.py
@@ -13,27 +13,45 @@ class GalagaModel:
         self.fighter = Fighter()
         self.bullets =[]
         self.basicEnemies = []
+
     def newBullet():
         self.bullets.append(Bullet())
         
+    def update():
+        for basicEnemy in self.basicEnemies:
+            basicEnemy.update()
+        
+        for bullet in self.bullets:
+            bullet.update()
+            
+        self.fighter.update()
+
 class BasicEnemy:
-    def __init__(self, x, y):
+    def __init__(self, x, y, vx, vy):
         self.x = x
         self.y = y
-        
+
+    def update():
+        self.x += self.vx
+        self.y += self.vy
+
 class Fighter:
-    def __init__(self):
+    def __init__(self, x, y):
         self.lives(3)
-        self.vx = 0.0
-        
+        self.x = x
+        self.y = y
 
 class Bullet:
-    def __init__(self,color,height,width,x,y):
+    def __init__(self,color,height,width,x,y,vy):
         self.color = color
         self.height = height
         self.width = width
         self.x = x
         self.y = y
+        self.vy = vy
+        
+    def update():
+        self.y += self.vy
 
 class PyGameWindowView:
     """ A view of brick breaker rendered in a Pygame window """
@@ -43,9 +61,24 @@ class PyGameWindowView:
         
     def draw(self):
         self.screen.fill(pygame.Color(0,0,0))
+        for basicEnemy in self.model.basicEnemies:
+            self.drawEnemy(basicEnemy)
+            
         for bullet in self.model.bullets:
-            pygame.draw.rect(self.screen, pygame.Color(bullet.color[0],bullet.color[1],bullet.color[2]),pygame.Rect(bullet.x,bullet.y,bullet.width,bullet.height))  
+            self.drawBullet(bullet)
+
+        self.drawFighter(self.model.fighter)
+        
         pygame.display.update()
+        
+    def drawEnemy(basicEnemy):
+        print "draw enemy please"
+        
+    def drawBullet(bullet):
+        print "draw bullet please"
+        
+    def drawFighter(fighter):
+        print "draw fighter please"
 
 class PyGameKeyboardController:
     """ Handles keyboard input for brick breaker """
@@ -56,11 +89,8 @@ class PyGameKeyboardController:
         if event.type != KEYDOWN:
             return
         if event.key == pygame.K_a:
-            self.model.fighter.vx += -1.0
+            self.model.fighter.x += -1.0
         if event.key == pygame.K_d:
-            self.model.fighter.vx += 1.0
+            self.model.fighter.x += 1.0
         if event.key == pygame.K_SPACE:
-            self.model.newBullet()
-
-    
-    
+            self.model.newBullet()    

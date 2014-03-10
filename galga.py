f201c43f00e70e3bdfcdb8fa56cafcf0f160b9c6
elizabeth ocallaghan <eocallaghan@eocallaghan-Latitude-E6430.(none)>
2014-03-10 01:05:30 -0400

    Added doc strings and fixed quit error.

--- galga.py
+++ galga.py
@@ -12,8 +12,12 @@ import math
 import time
 
 class GalagaModel:
-    """ Encodes the game state of Galaga"""
+    """ Encodes the game state of Galaga and keeps track of all attributes
+        contains a fighter, a list of bullets fired by my fighter, a list of enemies
+        and a list of bullets fired by the enemy"
+    """
     def __init__(self):
+        """ contructor for the GalagaModel class"""
         self.fighter = Fighter(350-17,660)
         self.myBullets = []
         self.enemyBullets = []
@@ -24,10 +28,12 @@ class GalagaModel:
             self.basicEnemies.append(basicEnemy)
 
     def newBullet(self, x,y):
+        """this function creates a new bullet fired from fighter at the passed in locations, x and y"""
         color = (255,0,0)
         self.myBullets.append(Bullet(color,15,5,x,y,-.75))
 
     def update(self):
+        """updates fighter, all bullets, and all enemies"""
         for bullet in self.myBullets:
             bullet.update()
         for bullet in self.enemyBullets:
@@ -40,40 +46,47 @@ class GalagaModel:
         self.fighter.update()
         
     def shoot(self, enemy):
+        """creates a new bullet fired by an enemy from the middle of the enemy"""
         color = (255, 255, 255)
         bullet = Bullet(color, 15,5,enemy.x+enemy.width/2,enemy.y+enemy.height,.75)
         self.enemyBullets.append(bullet)
         enemy.lastShootTime = time.time()
 
 class BasicEnemy:
+    """basic enemy class contains a constructor and an update method"""
     def __init__(self, x, y):
+        """contructor for basic enemy sets location based on passed in variables, and loads the picture
+        of an enemy from given file. also sets last shootTime"""
         self.x = x
         self.y = y
         self.height = 35
         self.width = 31
-        self.color = (255,255,255)
         self.vx = 0
         self.vy = 0
         self.image = pygame.image.load("galgaBasicEnemyShip.jpg")
         self.lastShootTime = time.time()
 
     def update(self):
+        """moves position based on current velocity"""
         self.x += self.vx
         self.y += self.vy
 
 class Fighter:
-
+    """fighter class contains a contruction and update method"""
     def __init__(self, x, y):
+        """constructor for fighter sets location based on given values, number of lives, pixle height
+        and width inital speed as zero and loads the image of a fighter"""
         self.lives = 1
         self.x = x
         self.y = y
         self.vx = 0.0
-        self.color = (250,250,0)
         self.height = 40
         self.width = 34
         self.image = pygame.image.load("galaga_ship.jpg")
 
     def update(self):
+        """moves position based on velocity unless at either side of screen and attempting to move
+        off screen where it is stopped"""
         if (self.x == 0 and self.vx < 0):
             self.vx = 0            
         elif(self.x == 700-self.width and self.vx>0):
@@ -82,8 +95,9 @@ class Fighter:
         
 
 class Bullet:
-    
+    """bullet class contains a constructor and update"""
     def __init__(self,color,height,width,x,y,vy):
+        """sets color, height, width, position, and velocity based on passed in parameters"""
         self.color = color
         self.height = height
         self.width = width
@@ -92,6 +106,7 @@ class Bullet:
         self.vy = vy
         
     def update(self):
+        """moves position based on velocity"""
         self.y += self.vy
 
 class PyGameWindowView:
@@ -101,6 +116,8 @@ class PyGameWindowView:
         self.screen = screen
         
     def draw(self):
+        """draws all of the elements on the screen uses a series of subfunctions
+        to draw fighters, bullets, and enemy"""
         self.screen.fill(pygame.Color(0,0,0))
         for basicEnemy in self.model.basicEnemies:
             self.drawEnemy(basicEnemy)
@@ -115,22 +132,30 @@ class PyGameWindowView:
         pygame.display.update()
 
     def drawEnemy(self, basicEnemy):
+        """draws an emeny from what is passed in"""
         screen.blit(basicEnemy.image,(basicEnemy.x,basicEnemy.y))
         
     def drawBullet(self, bullet):
+        """draws a rectangle for a bullet based on passed in bullet and its parameters"""
         rectangle = pygame.Rect(bullet.x,bullet.y,bullet.width,bullet.height)
         pygame.draw.rect(self.screen, bullet.color, rectangle)
         
     def drawFighter(self, fighter):
+        """draws a fighter from what is passed in"""
         screen.blit(fighter.image,(fighter.x,fighter.y))
 
 class PyGameKeyboardController:
-    """ Handles keyboard input for brick breaker """
+    """ Handles keyboard input for galaga"""
     def __init__(self,model):
+        """contructor just sets the model to model"""
         self.model = model
     
     def handle_keyboard_event(self,event):
+        """sets how the fighter moves and if the user shoots a bullet"""
         if event.type == KEYDOWN:
+            """if the a key is pushed and held down the fighter moves to the left if the d
+            key is pushed and held down the fighter moves to the right if the space bar is hit
+            a bullet is created"""
             if event.key == pygame.K_a:
                 self.model.fighter.vx = -1.0
             if event.key == pygame.K_d:
@@ -140,6 +165,8 @@ class PyGameKeyboardController:
                 y = self.model.fighter.y
                 model.newBullet(x,y)
         if event.type == KEYUP:
+            """if the a key is lifted up and the player is traveling to the left the player will stop.
+            if the d key is lifted and the player is traveling to the right the player will stop."""
             if event.key == pygame.K_a:
                 if (self.model.fighter.vx == -1):
                     self.model.fighter.vx = 0
@@ -148,23 +175,31 @@ class PyGameKeyboardController:
                     self.model.fighter.vx = 0            
 
 class CollisionController:
+    """tests if any two objects colloide"""
     def __init__(self, model):
         self.model = model
 
     def checkCollisions(self):
         fighter = self.model.fighter
         if (len(self.model.basicEnemies) == 0):
+            """if out of enemies the window closes and comand window prints score and that
+            user won"""
             print "Game Over! You Won"
             print "Score: "
             print self.model.score
             return False
         for basicEnemy in self.model.basicEnemies:
             for bullet in self.model.myBullets:
+                """checks if any bullets and enemies occupy the same space and removes them from the
+                model if they do occupy the same space"""
                 if (self.sameSpace(bullet.x, bullet.y, bullet.width, bullet.height, basicEnemy.x, basicEnemy.y, basicEnemy.width, basicEnemy.height)):              
                     self.model.myBullets.remove(bullet)
                     self.model.basicEnemies.remove(basicEnemy)
                     self.model.score += 10
             if (self.sameSpace(fighter.x, fighter.y, fighter.width, fighter.height, basicEnemy.x, basicEnemy.y, basicEnemy.width, basicEnemy.height)):
+                """checks if the fighter is in the same place as any of the basic enemies and removes the enemy and takes away a life from the 
+                    fighter if this is the case. if the fighter has no more lifes closes window and prints that the player 
+                    lost and prints the score"""
                 self.model.basicEnemies.remove(basicEnemy)
                 self.model.fighter.lives += -1
                 if (self.model.fighter.lives == 0):
@@ -173,6 +208,9 @@ class CollisionController:
                     print self.model.score
                     return False
         for bullet in self.model.enemyBullets:
+            """checks if the fighter is in the same place as any of the enemy bullets and removes the enemy bullet and takes away a life from the 
+                    fighter if this is the case. if the fighter has no more lifes closes window and prints that the player 
+                    lost and prints the score"""
             if (self.sameSpace(bullet.x, bullet.y, bullet. width, bullet.height, fighter.x, fighter.y, fighter.width, fighter.height)):              
                 self.model.enemyBullets.remove(bullet)
                 self.model.fighter.lives += -1
@@ -184,18 +222,24 @@ class CollisionController:
         return True
     
     def sameSpace(self, x1, y1, width1, height1, x2, y2, width2, height2):
+        """function checks if thetwo objects occupy the same space."""
         return (x2<= x1+width1 and x2+width2>= x1 and y2 <= y1+height2 and y2+height2 >= y1)
         
 class EnemyController:
+    """controls all the enemies on the screen.  Has a constructor, move enemy function, 
+    and should shoot function"""
     def __init__(self, model):
         self.model = model
 
     def moveEnemy(self):
+        """checks if the an enemy is moving.  If not, starts it to move.
+        If so it proceeds to the next one.  Returns once moves an enemy."""
         for enemy in self.model.basicEnemies:
             if (enemy.vy == 0):
                 enemy.vy = .5
                 return
     def shouldShoot(self):
+        """shoots if the enemy is moving and if it shot more than two seconds ago shoots again."""
         for enemy in self.model.basicEnemies:
             if(not enemy.vy == 0 and time.time()>enemy.lastShootTime +2):
                 self.model.shoot(enemy)
@@ -217,14 +261,14 @@ if __name__ == '__main__':
     startTime = time.time()
     
     while running:
-        if (time.time() > startTime+3):
+        if (time.time() > startTime+3):  #if an enemy moved more than three seconds ago, moves next enemy
             enemyController.moveEnemy()
             startTime = time.time()
+        running = collisionController.checkCollisions()
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
             KeyBoardcontroller.handle_keyboard_event(event)
-        running = collisionController.checkCollisions()
         enemyController.shouldShoot()
         model.update()
         view.draw()
